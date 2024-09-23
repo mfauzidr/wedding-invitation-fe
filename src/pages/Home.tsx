2c14fda03fd5cf3899febb22606db15d1f274641
mfauzidr <mfdr.fauzi97@gmail.com>
2024-09-23 13:09:39 +0700

    add name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,36 +15,36 @@ interface Username {
 
 const Home = ({ username }: Username) => {
   return (
-    <div className='flex flex-col w-full px-0 mx-0 items-center bg-maroon border-0 overflow-x-hidden'>
-      <section className='w-full' id="invitation">
+    <div className='flex flex-col w-full h-c px-0 mx-0 items-center bg-maroon border-0 overflow-x-hidden'>
+      <section className='w-full md:w-[414px]' id="invitation">
         <Invitation username={username} />
       </section>
 
-      <section className='w-full' id="countdown">
+      <section className='w-full md:w-[414px]' id="countdown">
         <Countdown />
       </section>
 
-      <section className='w-full' id="bridegroom">
+      <section className='w-full md:w-[414px]' id="bridegroom">
         <BrideGroom />
       </section>
 
-      <section className='w-full' id="wedding-info">
+      <section className='w-full md:w-[414px]' id="wedding-info">
         <WeddingInfo />
       </section>
 
-      <section className='w-full' id="love-story">
+      <section className='w-full md:w-[414px]' id="love-story">
         <LoveStory />
       </section>
 
-      <section className='w-full' id="gallery">
+      <section className='w-full md:w-[414px]' id="gallery">
         <Gallery />
       </section>
 
-      <section className='w-full' id="rsvp">
+      <section className='w-full md:w-[414px]' id="rsvp">
         <Rsvp />
       </section>
 
-      <section className='w-full' id="wedding-gift">
+      <section className='w-full md:w-[414px]' id="wedding-gift">
         <WeddingGift />
       </section>
       <Outlet />
--- src/pages/Invitation.tsx
+++ src/pages/Invitation.tsx
@@ -2,6 +2,8 @@
 import invitationImage from '../assets/images/image-2.jpg'
 import leftHead from '../assets/images/LeftHead.png'
 import rightHead from '../assets/images/RightHead.png'
+import ring from '../assets/images/Rings.png'
+import andLine from '../assets/images/andLine.png'
 
 
 interface Username {
@@ -17,13 +19,21 @@ const Invitation = ({ username }: Username) => {
           <img className='w-[77px] h-[90px]' src={rightHead} />
           <img className='w-[77px] h-[90px]' src={leftHead} />
         </div>
-        <div className="absolute right-0 top transform -translate-y-1/2 flex flex-col gap-0 divide-y w-fit text-2xl font-bellefair text-white pr-10 pt-2">
+        <div className="absolute right-0 top transform -translate-y-1/2 flex flex-col gap-0 divide-y w-fit text-3xl font-bellefair text-white pr-10 pt-2">
           <p>06</p>
           <p>10</p>
           <p>24</p>
         </div>
       </div>
-
+      <div className='relative flex flex-col -mt-28 items-center justify-center text-white'>
+        <img src={ring} className='w-[51px] h-[51px]' />
+        <p className='font-cinzel-decorative mt-[9px] text-2xl tracking-[.4em] pl-2'>fauzi</p>
+        <p className='font-cinzel-decorative mt-[21px] text-2xl tracking-[.4em] pl-2'>fauriza</p>
+        <p className='font-cinzel-decorative mt-[3px] text-sm/[11px] tracking-[.25em]'>wedding invitation</p>
+        <div className='relative -mt-[100px]'>
+          <img src={andLine} className='w-[310px] h-[123px] shadow-2xl' />
+        </div>
+      </div>
 
 
     </div>
